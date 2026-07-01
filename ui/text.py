748a43e50bb75b18524690fa7df5cf8f3e4ae39a
Jeo Mari Saez <jeomarisaez7@gmail.com>
2026-07-01 16:42:54 +0800

    chore: add text.py to git

---
+++ ui/text.py
@@ -0,0 +1,36 @@
+import pygame
+
+class TextUI:
+    COUNTER_SIZE = (100, 50)
+    BUST_SIZE = (100, 50)
+    WHITE = (255,255,255)
+    BLACK = (0,0,0)
+    BLUE = (50,50,255)
+
+    def __init__(self, screen):
+        self.screen = screen
+        self.score_font = pygame.font.SysFont("Arial", 30)
+
+    def draw_counter(self, hand, x, y):
+        rect = pygame.Rect(0, 0, *self.COUNTER_SIZE)
+        rect.center = (x, y)
+
+        text_surf = self.score_font.render(str(hand.get_value), True, self.WHITE)
+
+        self.screen.blit(text_surf, text_surf.get_rect(center=rect.center))
+
+    def draw_bust(self, player):
+        screen_rect = self.screen.get_rect()
+
+        text = f"{player.name} Busted!"
+        rect = pygame.Rect(0, 0, *self.COUNTER_SIZE)
+        rect.center = (screen_rect.centerx, screen_rect.centery)
+
+        text_surf = self.score_font.render(text, True, self.WHITE)
+
+        self.screen.blit(text_surf, text_surf.get_rect(center=rect.center))
+
+
+
+        
+
